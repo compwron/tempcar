37c80633840c487b150da3e60b581a943c113679
compwron <wordymusic@gmail.com>
2013-02-17 15:09:20 -0600

    Initial commit

---
+++ src/com/practice/carpod/ViewCars.java
@@ -0,0 +1,22 @@
+package com.practice.carpod;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ViewCars extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_cars);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_view_cars, menu);
+        return true;
+    }
+    
+}
